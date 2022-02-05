07a41379f7b8b25ef8198ee0b00237436071e494
chhaviteotia <chhavi.teotia8@gmail.com>
2022-02-05 13:33:10 +0530

    reactAssignment_2_commit_1

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -8,7 +8,22 @@ function App(props) {
     
 return(
   <div className="container">
-  {/* Start your React code here */}
+  
+    <div className="profile-container">
+      <img className="profile-pic" src={employee.profileImg} width="100" height="100" alt="profile-picture"/>
+  
+      <h1>{employee.name}</h1>
+
+      <p>Location</p>
+      <strong>{employee.location}</strong>
+
+      <p>Blood Group</p>
+      <strong>{employee.bloodGroup}</strong>
+
+      <p>Age</p>
+      <strong>{employee.age}</strong>
+    </div>
+  
 </div>
 )
 }
